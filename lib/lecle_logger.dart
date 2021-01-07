009970b92acf868bd69c65ff4e3cbe3184a0a70f
kthangtd <kthangtd@lecle.co.kr>
2021-01-07 08:34:15 +0700

    fixed log crash

--- lib/lecle_logger.dart
+++ lib/lecle_logger.dart
@@ -7,7 +7,7 @@ part 'logger_srv.dart';
 
 Future initLogSrv({bool enabled = true}) async {
   assert(enabled != null);
-  return _LogService.init();
+  return _LogService.init(enabled: enabled);
 }
 
 _LogService get logSrv => _LogService.shared();
